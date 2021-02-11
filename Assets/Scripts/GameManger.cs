652362b81cc2c262d5ed259b5452bd2e59abdd0a
Chaitanya Estarla <35031758+ChaitanyaEstarla@users.noreply.github.com>
2021-02-11 17:49:40 +0530

    Full Backup
    Completed Cinemachine and Particle creation

---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    public GameObject dollyCam;
+
+    void Start()
+    {
+        StartCoroutine(IntroShot());
+    }
+
+
+    IEnumerator IntroShot()
+    {
+        yield return new WaitForSeconds(11);
+        dollyCam.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform player;
+    public float speed = 0;
+    
+    private float mouseSensitivity = 1000f;
+    public Transform playerBody;
+    public Transform playerEyes;
+
+    private float mouseX;
+    private float mouseY;
+    
+    void Start()
+    {
+        player = GetComponent<Transform>();
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.position -= transform.right * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.position += transform.right * Time.deltaTime * speed;
+        }
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.position += transform.forward * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.L))
+        {
+            Cursor.visible = true;
+        }
+        
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        playerBody.Rotate(Vector3.up * mouseX);
+        playerEyes.Rotate(Vector3.left * mouseY);
+    }
+}
