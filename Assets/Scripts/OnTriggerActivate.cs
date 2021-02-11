ee69f614f805cbf3a13ec5352f29f4e79a801ff5
Chaitanya Estarla <35031758+ChaitanyaEstarla@users.noreply.github.com>
2021-02-11 18:28:39 +0530

    Evening Backup
    Started Scripting Text for Particles

---
+++ Assets/Scripts/OnTriggerActivate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerActivate : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
